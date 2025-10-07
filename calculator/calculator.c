62e4b6f232f732db9845d2ae5c9e6646e9277e37
Guptaji2003 <pulkit7976@gmail.com>
2025-10-07 15:50:12 +0530

    second commit

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -3,74 +3,103 @@
 #include <string.h>
 #include <ctype.h>
 
-int pri(char c) {
-    if(c=='+' ||c=='-')  return 1;
-    if( c=='*'|| c== '/') return 2;
+#define MAX 100
+
+int findprecedence(char op)
+{
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
     return 0;
 }
 
-int calc(int a,int b,char c) {
-    if( c=='+') return a+b;
-    if(c=='-') return a-b;
-    if(c=='*') return a*b;
-    if(c=='/') {
-        if(b==0) {
-            printf("error- divide by 0\n");
-            exit(0);
+int performOperation(int a, int b, char op)
+{
+    if (op == '+')
+        return a + b;
+    if (op == '-')
+        return a - b;
+    if (op == '*')
+        return a * b;
+    if (op == '/')
+    {
+        if (b == 0)
+        {
+            printf("error: division by 0\n");
+            return 0;
         }
-        return a/b;
+        return a / b;
     }
-    return 0;
+    else
+    {
+        printf("error: invalid operator '%c'\n", op);
+        return 0;
+    }
 }
 
-int eval(char s[]) {
-    int val[100];
-    int vt=-1;
-    char op[100]; 
-    int ot=-1;
+int evaluate(char s[])
+{
+    int valuestack[MAX];
+    int valuetop = -1;
+    char operatorstack[MAX];
+    int operatortop = -1;
 
-    for(int i=0; i<strlen(s);i++) {
-        if(s[i]==' ') continue;
+    int len = strlen(s);
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == ' ')
+            continue;
 
-        if(isdigit(s[i])) {
-            int n=0;
-            while(i<strlen(s)&&isdigit(s[i])) {
-                n=n*10+(s[i]-'0');
+        if (isdigit(s[i]))
+        {
+            int n = 0;
+            while (i < len && isdigit(s[i]))
+            {
+                n = n * 10 + (s[i] - '0');
                 i++;
             }
-
-            val[++vt] = n;
+            valuestack[++valuetop] = n;
             i--;
-        } else if(s[i]=='+'|| s[i]=='-'|| s[i]=='*'||s[i]=='/') {
-            while(ot>=0 && pri(op[ot])>=pri(s[i])) {
-                int b=val[vt--];
-                int a=val[vt--];
-                char c=op[ot--];
-                
-                val[++vt]=calc(a,b,c);
+        }
+        else if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/')
+        {
+            while (operatortop >= 0 && findprecedence(operatorstack[operatortop]) >= findprecedence(s[i]))
+            {
+                int b = valuestack[valuetop--];
+                int a = valuestack[valuetop--];
+                char c = operatorstack[operatortop--];
+
+                int ans = performOperation(a, b, c);
+                valuestack[++valuetop] = ans;
             }
-            op[++ot]=s[i];
-        } else {
-            printf("error: invalid expression\n");
-            exit(0);
+            operatorstack[++operatortop] = s[i];
+        }
+        else
+        {
+            printf("error: invalid character '%c'\n", s[i]);
+            return 0;
         }
     }
-    while(ot>=0) {
-        int b=val[vt--];
-        int a =val[vt--];
-        char c =op[ot--];
 
-        val[++vt] = calc(a,b,c);
+    while (operatortop >= 0)
+    {
+        int b = valuestack[valuetop--];
+        int a = valuestack[valuetop--];
+        char c = operatorstack[operatortop--];
+        int ans = performOperation(a, b, c);
+        valuestack[++valuetop] = ans;
     }
-    return val[vt];
+    return valuestack[valuetop];
 }
 
-int main() {
-    char exp[200];
-    printf("enter your expression : ");
-    fgets(exp,200,stdin);
-    exp[strcspn(exp,"\n")]=0;
-    int ans=eval(exp);
-    printf("ans is %d\n",ans);
+int main()
+{
+    char exp[MAX];
+    printf("enter expression: ");
+    fgets(exp, MAX, stdin);
+    exp[strcspn(exp, "\n")] = 0;
+    int result = evaluate(exp);
+    printf("Result: %d\n", result);
     return 0;
 }

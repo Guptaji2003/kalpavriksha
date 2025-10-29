c6f6b0b574e750e3f751a6b5a93faeb8581e1c67
Guptaji2003 <pulkit7976@gmail.com>
2025-10-29 12:51:25 +0530

    7th commit

--- Dynamic Inventory System/dynamic_inventory_system.c
+++ Dynamic Inventory System/dynamic_inventory_system.c
@@ -88,7 +88,7 @@ void productPriceValidation(float *productPrice)
     while (1)
     {
         printf("Enter product price (0-100000): ");
-        if (scanf("%f%c", productPrice,&extrachar) != 2 || extrachar!='\n')
+        if (scanf("%f%c", productPrice, &extrachar) != 2 || extrachar != '\n')
         {
             printf("Invalid input. Please enter a valid number.\n");
             while (getchar() != '\n')
@@ -108,13 +108,13 @@ void productPriceValidation(float *productPrice)
 }
 void minPriceValidation(float *minPrice)
 {
-        char extrachar;
+    char extrachar;
 
     while (1)
     {
-     
+
         printf("Enter minimum price: ");
-        if (scanf("%f%c", minPrice,&extrachar) != 2 || extrachar!='\n')
+        if (scanf("%f%c", minPrice, &extrachar) != 2 || extrachar != '\n')
         {
             printf("Invalid input. Please enter a valid number.\n");
             while (getchar() != '\n')
@@ -138,7 +138,7 @@ void maxPriceValidation(float *maxPrice, float minPrice)
     while (1)
     {
         printf("Enter maximum price: ");
-        if (scanf("%f%c", maxPrice,&extrachar) != 2 || extrachar!='\n')
+        if (scanf("%f%c", maxPrice, &extrachar) != 2 || extrachar != '\n')
         {
             printf("Invalid input. Please enter a valid number.\n");
             while (getchar() != '\n')
@@ -310,36 +310,111 @@ void searchProductById(struct Product *products, int productCount)
         printf("\nProduct not found.\n");
     }
 }
-void searchProductByName(struct Product *products, int productCount)
+
+static int containsIgnoreCase(const char *mainString, const char *searchSubstring)
 {
-    char name[50];
-    int found = 0;
+    size_t mainLength = strlen(mainString);
+    size_t searchLength = strlen(searchSubstring);
 
-    printf("Enter product name to search: ");
-    getchar();
-    fgets(name, sizeof(name), stdin);
-    name[strcspn(name, "\n")] = '\0';
+    if (searchLength == 0 || mainLength < searchLength)
+        return 0;
 
-    if (strlen(name) == 0)
+    for (size_t mainIndex = 0; mainIndex <= mainLength - searchLength; ++mainIndex)
     {
-        printf("Product name cannot be empty!\n");
-        return;
+        size_t matchIndex;
+        for (matchIndex = 0; matchIndex < searchLength; ++matchIndex)
+        {
+            if (tolower((unsigned char)mainString[mainIndex + matchIndex]) !=
+                tolower((unsigned char)searchSubstring[matchIndex]))
+                break;
+        }
+        if (matchIndex == searchLength)
+            return 1;
+    }
+    return 0;
+}
+
+static int isEmptyOrSpaces(const char *inputString)
+{
+    for (size_t charIndex = 0; inputString[charIndex] != '\0'; ++charIndex)
+        if (!isspace((unsigned char)inputString[charIndex]))
+            return 0;
+    return 1;
+}
+
+void searchProductByName(struct Product *products, int totalProducts)
+{
+    char searchName[51];
+    int isProductFound = 0;
+
+    while (1)
+    {
+        printf("Enter product name to search: ");
+
+        if (!fgets(searchName, sizeof(searchName), stdin))
+            return;
+
+        if (searchName[0] == '\n')
+        {
+            printf("Product name cannot be empty!\n");
+            continue;
+        }
+
+        size_t inputLength = strlen(searchName);
+
+        if (inputLength > 0 && searchName[inputLength - 1] != '\n')
+        {
+            int extraChar, inputTooLong = 0;
+            while ((extraChar = getchar()) != '\n' && extraChar != EOF)
+                inputTooLong = 1;
+            if (inputTooLong)
+            {
+                printf("Input too long! Maximum 50 characters allowed.\n");
+                continue;
+            }
+        }
+        else if (inputLength > 0 && searchName[inputLength - 1] == '\n')
+        {
+            searchName[inputLength - 1] = '\0';
+            inputLength--;
+        }
+
+        while (inputLength > 0 && isspace((unsigned char)searchName[inputLength - 1]))
+        {
+            searchName[inputLength - 1] = '\0';
+            inputLength--;
+        }
+
+        if (inputLength == 0 || isEmptyOrSpaces(searchName))
+        {
+            printf("Product name cannot be empty or spaces only!\n");
+            continue;
+        }
+
+        break;
     }
 
-    for (int productIndex = 0; productIndex < productCount; productIndex++)
+    for (int productIndex = 0; productIndex < totalProducts; ++productIndex)
     {
-        if (strcasecmp((products + productIndex)->product_name, name) == 0)
+        if (containsIgnoreCase((products + productIndex)->product_name, searchName))
         {
-            found = 1;
-            printf("Product ID: %d | Name: %s | Price: %.2f | Quantity: %d\n", (products + productIndex)->product_id, (products + productIndex)->product_name, (products + productIndex)->product_price, (products + productIndex)->product_quantity);
+            isProductFound = 1;
+            printf("Product ID: %d | Name: %s | Price: %.2f | Quantity: %d\n",
+                   (products + productIndex)->product_id,
+                   (products + productIndex)->product_name,
+                   (products + productIndex)->product_price,
+                   (products + productIndex)->product_quantity);
         }
     }
 
-    if (found)
-        printf("\nProducts found successfully.\n");
+    if (!isProductFound)
+        printf("No product found containing \"%s\".\n", searchName);
     else
-        printf("\nNo product found with name \"%s\".\n", name);
+        printf("Matching products displayed.\n");
 }
+
+
+
 
 void searchProductByPrice(struct Product *products, int productCount)
 {
@@ -427,6 +502,19 @@ void addInitialProducts(struct Product *products, int inventoryLength)
     printf("\nAll initial products added successfully!\n");
 }
 
+void printMenu()
+{
+    printf("\n========= INVENTORY MENU =========\n");
+    printf("1. Add New Product \n");
+    printf("2. Display All Products \n");
+    printf("3. Update Product Quantity \n");
+    printf("4. Search Product by ID \n");
+    printf("5. Search Product by Name \n");
+    printf("6. Search Product by Price Range \n");
+    printf("7. Delete Product \n");
+    printf("8. Exit \n");
+}
+
 int main()
 {
     int choice;
@@ -455,48 +543,52 @@ int main()
     printf("8. Exit \n");
     while (1)
     {
+        printMenu();
         char extra;
         printf("Enter your choice: ");
-        if (scanf("%d%c", &choice,&extra) != 2 || extra!='\n')
+        if (scanf("%d%c", &choice, &extra) != 2 || extra != '\n')
         {
             printf("Invalid input, Please enter a number between 1-8.\n");
             while (getchar() != '\n')
                 ;
             continue;
-        }else{
-
-        switch (choice)
-        {
-        case 1:
-            addNewProduct(&productIndex, &inventoryLength, &products);
-            break;
-        case 2:
-            showAllProducts(productIndex, products);
-            break;
-        case 3:
-            updateProductQuantity(products, productIndex);
-            break;
-        case 4:
-            searchProductById(products, productIndex);
-            break;
-        case 5:
-            searchProductByName(products, productIndex);
-            break;
-        case 6:
-            searchProductByPrice(products, productIndex);
-            break;
-        case 7:
-            deleteProductById(products, &productIndex);
-            break;
-        case 8:
-            free(products);
-            printf("\nMemory released successfully. Exiting program...\n");
-            return 0;
-        default:
-            printf("Invalid choice. Please try again.\n");
-            continue;
         }
-    }
+        else
+        {
+
+            switch (choice)
+            {
+            case 1:
+                addNewProduct(&productIndex, &inventoryLength, &products);
+                break;
+            case 2:
+                showAllProducts(productIndex, products);
+                break;
+            case 3:
+                updateProductQuantity(products, productIndex);
+                break;
+            case 4:
+                searchProductById(products, productIndex);
+                break;
+            case 5:
+                searchProductByName(products, productIndex);
+                break;
+            case 6:
+                searchProductByPrice(products, productIndex);
+                break;
+            case 7:
+                deleteProductById(products, &productIndex);
+                break;
+            case 8:
+                free(products);
+                products = NULL;
+                printf("\nMemory released successfully. Exiting program...\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                continue;
+            }
+        }
     }
     return 0;
 }
